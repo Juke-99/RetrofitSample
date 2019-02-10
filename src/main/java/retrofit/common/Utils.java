8ff8e19947c0320313024bc46e489ad4ec61bbd3
Juke-99 <yanpahika@gmail.com>
2019-02-11 03:13:38 +0900

    2019/02/11

--- src/main/java/retrofit/common/Utils.java
+++ src/main/java/retrofit/common/Utils.java
@@ -1,8 +1,13 @@
 package retrofit.common;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.lang.reflect.WildcardType;
+
+import javax.annotation.Nullable;
 
 public final class Utils {
 	public static Type getParameterUpperBound(int index, ParameterizedType type) {
@@ -20,5 +25,50 @@ public final class Utils {
 	    }
 	    
 	    return paramType;
+	}
+	
+	public static Class<?> getRawType(Type type) {
+	    checkNotNull(type, "type == null");
+
+	    if (type instanceof Class<?>) {
+	      // Type is a normal class.
+	      return (Class<?>) type;
+	    }
+	    
+	    if (type instanceof ParameterizedType) {
+	      ParameterizedType parameterizedType = (ParameterizedType) type;
+
+	      // I'm not exactly sure why getRawType() returns Type instead of Class. Neal isn't either but
+	      // suspects some pathological case related to nested classes exists.
+	      Type rawType = parameterizedType.getRawType();
+	      if (!(rawType instanceof Class)) throw new IllegalArgumentException();
+	      return (Class<?>) rawType;
+	    }
+	    
+	    if (type instanceof GenericArrayType) {
+	      Type componentType = ((GenericArrayType) type).getGenericComponentType();
+	      return Array.newInstance(getRawType(componentType), 0).getClass();
+	    }
+	    
+	    if (type instanceof TypeVariable) {
+	      // We could use the variable's bounds, but that won't work if there are multiple. Having a raw
+	      // type that's more general than necessary is okay.
+	      return Object.class;
+	    }
+	    
+	    if (type instanceof WildcardType) {
+	      return getRawType(((WildcardType) type).getUpperBounds()[0]);
+	    }
+
+	    throw new IllegalArgumentException("Expected a Class, ParameterizedType, or "
+	          + "GenericArrayType, but <" + type + "> is of type " + type.getClass().getName());
+	}
+	
+	static <T> T checkNotNull(@Nullable T object, String message) {
+	    if (object == null) {
+	      throw new NullPointerException(message);
+	    }
+	    
+	    return object;
 	}
 }
--- src/test/java/retrofit/calladapter/CallAdapterTest.java
+++ src/test/java/retrofit/calladapter/CallAdapterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.fail;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import org.junit.Test;
@@ -27,8 +28,43 @@ public class CallAdapterTest {
 		try {
 			Utils.getParameterUpperBound(1, StringList);
 		    fail();
-		} catch (IllegalArgumentException e) {
-		    assertThat(e).hasMessage("Index 1 not in range [0,1) for java.util.List<java.lang.String>");
+		} catch (IllegalArgumentException iae) {
+		    assertThat(iae).hasMessage("Index 1 not in range [0,1) for java.util.List<java.lang.String>");
+		}
+	}
+	
+	@Test
+	public void rowTypes() throws NoSuchMethodException, SecurityException {
+		assertThat(Utils.getRawType(String.class)).isSameAs(String.class);
+		
+		Type listString = new TypeToken<List<String>>() {
+			private static final long serialVersionUID = 1L;
+		}.getType();
+		assertThat(Utils.getRawType(listString)).isSameAs(List.class);
+		
+		Type arrayString = new TypeToken<String[]>() {
+			private static final long serialVersionUID = 1L;
+		}.getType();
+		assertThat(Utils.getRawType(arrayString)).isSameAs(String[].class);
+		
+		Type wild = ((ParameterizedType) new TypeToken<List<? extends CharSequence>>() {
+			private static final long serialVersionUID = 1L;
+	    }.getType()).getActualTypeArguments()[0];
+	    assertThat(Utils.getRawType(wild)).isSameAs(CharSequence.class);
+
+	    Type wildParam = ((ParameterizedType) new TypeToken<List<? extends List<String>>>() {
+			private static final long serialVersionUID = 1L;
+	    }.getType()).getActualTypeArguments()[0];
+	    assertThat(Utils.getRawType(wildParam)).isSameAs(List.class);
+
+	    Type typeVar = A.class.getDeclaredMethod("method").getGenericReturnType();
+	    assertThat(Utils.getRawType(typeVar)).isSameAs(Object.class);
+	}
+	
+	@SuppressWarnings("unused")
+	static private class A<T> {
+		T method() {
+			return null;
 		}
 	}
 }
